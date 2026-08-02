0c390a1d8cd0c7c62d3f7455706878c781c154ef
kavin <kavinr2400@gmail.com>
2026-08-01 18:21:48 -0700

    feat: add aplay support for playing audio on linux/rpi

--- alerts/alerts.py
+++ alerts/alerts.py
@@ -1,7 +1,11 @@
+import os
+import subprocess
 from pathlib import Path
 
 import pyttsx3
-import winsound
+
+if os.name == "nt":
+    import winsound
 
 
 YAWN_WARNING_MESSAGE = "Yawning detected. Please stay alert."
@@ -32,9 +36,20 @@ def speak_distraction_warning():
 
 
 def play_audio_file(wait=False):
-    flags = winsound.SND_FILENAME | winsound.SND_NODEFAULT
-    flags |= winsound.SND_SYNC if wait else winsound.SND_ASYNC
-    winsound.PlaySound(str(ALERT_AUDIO_FILE), flags)
+    audio_path = str(ALERT_AUDIO_FILE)
+
+    if os.name == "nt":
+        flags = winsound.SND_FILENAME | winsound.SND_NODEFAULT
+        flags |= winsound.SND_SYNC if wait else winsound.SND_ASYNC
+        winsound.PlaySound(audio_path, flags)
+    else:
+        process = subprocess.Popen(
+            ["aplay", audio_path],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        if wait:
+            process.wait()
 
 
 if __name__ == "__main__":
